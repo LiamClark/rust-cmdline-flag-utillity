f5c9717f9e1213c1f6b737ca167b13041c2fc932
LiamClark <L.A.Clark@student.tudelft.nl>
2016-02-27 23:31:15 +0100

    format parser is done now

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 mod tests;
 
 use std::env;
-use std::collections::HashMap;
 
 fn main() {
     let flags : Vec<_> = env::args().skip(1).collect();
@@ -9,47 +8,55 @@ fn main() {
 }
 
 struct FlagFormat {
-    bools   : HashMap<String,bool>,
-    ints    : HashMap<String,i32>,
-    // strings : HashMap<String,String>,
+    bools   : Vec<String>,
+    ints    : Vec<String>,
+    strings : Vec<String>,
 }
 
 impl FlagFormat {
-    pub fn parse<S: Into<String>>(flag: S) -> FlagFormat {
-        let flag_string = flag.into();
-        let mut bools   =   HashMap::new();
-        let mut ints    =   HashMap::new();
-        let mut strings =   HashMap::new();
+    pub fn parse(flag: &str) -> FlagFormat {
+        let mut bools   =   Vec::new();
+        let mut ints    =   Vec::new();
+        let mut strings =   Vec::new();
 
-        for s in flag_string.trim().split(',') {
-            let (name, postfix) = FlagFormat::split_argument_specifier(s.into());
+        for s in flag.split(',') {
+            let (name, postfix) = FlagFormat::split_argument_specifier(s);
+            let name = name.into();
             match postfix {
-                None => { bools.insert(name,false); }
-                Some(x) => match x.trim() {
-                     "#" => { ints.insert(name, 3); }
-                     "*" => { strings.insert(name, String::from("hello")); }
+                None => { bools.push(name); }
+                Some(x) => match x {
+                     "#" => { ints.push(name); }
+                     "*" => { strings.push(name); }
                      _ => {}
                 }
             };
         }
-        FlagFormat{bools: bools, ints: ints}
+        FlagFormat{bools: bools, ints: ints, strings: strings}
     }
 
-    fn split_argument_specifier(flag_string: String) -> (String, Option<String>) {
-        let size = flag_string.capacity();
+    fn split_argument_specifier(flag_string: &str) -> (&str, Option<&str>) {
+        let size = flag_string.len();
         if size == 1 {
             (flag_string, None)
         } else {
             let (start, end) =  flag_string.split_at(size -1);
-            (start.into(), Some(end.into()))
+            (start, Some(end))
         }
     }
 
-    pub fn getBool(&self, s :&String) -> Option<&bool> {
-        self.bools.get(s)
+    pub fn is_bool_arg(&self, s : &str) -> bool {
+        FlagFormat::is_arg(&self.bools, s)
     }
 
-    pub fn getInt(&self, s: &String) -> Option<&i32> {
-        self.ints.get(s)
+    pub fn is_int_arg(&self, s : &str) -> bool {
+        FlagFormat::is_arg(&self.ints, s)
+    }
+
+    pub fn is_string_arg(&self, s : &str) -> bool {
+        FlagFormat::is_arg(&self.strings, s)
+    }
+
+    fn is_arg(args: &Vec<String>, s: &str) -> bool {
+        args.iter().find(|name| name.trim() == s).is_some()
     }
 }
--- src/tests.rs
+++ src/tests.rs
@@ -3,26 +3,31 @@ use FlagFormat;
 
 #[test]
 fn parse_multi_test_one_letter(){
-    let expected = ("p".to_string(),None);
-    assert_eq!(FlagFormat::split_argument_specifier("p".to_string()), expected);
+    let expected = ("p", None);
+    assert_eq!(FlagFormat::split_argument_specifier("p"), expected);
 }
 
 #[test]
 fn multi_letter_test() {
-    let expected = ("p".to_string(),Some("*".to_string()));
-    assert_eq!(FlagFormat::split_argument_specifier("p*".to_string()),expected)
+    let expected = ("p", Some("*"));
+    assert_eq!(FlagFormat::split_argument_specifier("p*"),expected)
 }
 
 #[test]
 fn integration_test_bool() {
     let args = FlagFormat::parse("p*, l#,r");
-    let arg = args.getBool(&"r".to_string());
-    assert!(!arg.unwrap())
+    let arg = args.is_bool_arg(&"r");
+    assert!(arg)
 }
-
 
 #[test]
 fn integration_test_int() {
     let args = FlagFormat::parse("p*,l#,r");
-    assert_eq!(args.getInt(&String::from("l")).unwrap(), &3)
+    assert!(args.is_int_arg(&"l"))
+}
+
+#[test]
+fn integration_test_string() {
+    let args = FlagFormat::parse("p*,l#,r");
+    assert!(args.is_string_arg(&"p"))
 }
